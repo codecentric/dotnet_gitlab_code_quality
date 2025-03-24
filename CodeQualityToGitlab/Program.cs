759acb085a5d06754927ae7c6d0549731c1f402c
Christian Sauer <christian.sauer@codecentric.de>
2025-03-24 08:49:52 +0100

    feat: default to cwd for current directory

--- CodeQualityToGitlab/Program.cs
+++ CodeQualityToGitlab/Program.cs
@@ -27,7 +27,7 @@ internal static class Program
             description: "The name root of the repository. Gitlab requires Code Quality issues to contain paths relative to the repository, "
                 + "but the tools report them as absolute file paths. "
                 + "Everything given in with this option will be removed. E.g. root is 'c:/dev' and the file name is something like 'c:/dev/myrepo/file.cs' it will transformed to 'myrepo/file.cs'. Can often be omitted. ",
-            getDefaultValue: () => null
+            getDefaultValue: Directory.GetCurrentDirectory
         );
 
         var rootCommand = new RootCommand("Tool to convert Dotnet-Formats to Gitlab code quality");
